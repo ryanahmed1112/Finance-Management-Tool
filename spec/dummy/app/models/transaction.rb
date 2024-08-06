cf9ddfe56d897db13d49cec2364d4555fa3d935a
ryanahmed1112 <backend.builder01@gmail.com>
2024-08-06 23:05:39 +0500

    final commit

---
+++ spec/dummy/app/models/transaction.rb
@@ -0,0 +1,13 @@
+class Transaction < ActiveRecord::Base
+  monetize :amount_cents, with_model_currency: :currency
+
+  monetize :tax_cents, with_model_currency: :currency
+
+  monetize :total_cents, with_model_currency: :currency
+
+  monetize :optional_amount_cents, with_model_currency: :currency, allow_nil: true
+
+  def total_cents(foo = 0, bar: 0)
+    amount_cents + tax_cents + foo + bar
+  end
+end
